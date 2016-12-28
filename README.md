fd8741e0dde1bc7cee75be31378a1b89bd2c6ad7
Scott <scottgangemi@gmail.com>
2016-12-29 10:12:35 +1100

    initial setup of repo and config of project

---
+++ README.md
@@ -0,0 +1,5 @@
+# markdown-preview
+
+Continual work on React, this project is to look similar to https://www.freecodecamp.com/challenges/build-a-recipe-box
+
+
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+var config = {
+    devtool: 'source-map',
+    entry: APP_DIR + '/index.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+
+    module: {
+        loaders: [{
+            test: /\.jsx?/,
+            include: APP_DIR,
+            loader: 'babel'
+        }]
+    },
+
+    // plugins: [
+    //     new webpack.optimize.OccurenceOrderPlugin(),
+    //     new webpack.DefinePlugin({
+    //         'process.env': {
+    //             'NODE_ENV': JSON.stringify('production'),
+    //         }
+    //     }),
+    //     new webpack.optimize.UglifyJsPlugin({
+    //         compress: {
+    //             warnings: false,
+    //         },
+    //         output: {
+    //             comments: false,
+    //         },
+    //     }),
+    // ]
+};
+
+module.exports = config;
